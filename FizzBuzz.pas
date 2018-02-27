1a157d686401a3db765e02cf86d5238dd1c0441e
aselivanov <aseliv[]gmail>
2018-02-27 19:29:20 +0300

    Initial commit - the code compiles and runs correctly.

---
+++ FizzBuzz.pas
@@ -0,0 +1,14 @@
+Program FizzBuzz(output);
+Var i: Integer;
+
+Begin
+    For i := 1 To 100 Do Begin
+        If i mod 3 = 0 Then
+            Write('Fizz');
+        If i mod 5 = 0 Then
+            Write('Buzz');
+        If (i mod 3 <> 0) and (i mod 5 <>0) Then
+            Write(i);
+        Write(' ');
+    End;
+End.
